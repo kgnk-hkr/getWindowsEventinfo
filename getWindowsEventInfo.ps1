81cae69297b632d5bd005832a382300b6bc1f9e1
kgnk-hkr <48877167+kgnk-hkr@users.noreply.github.com>
2020-11-23 20:19:16 +0900

    Update getWindowsEventInfo.ps1

--- getWindowsEventInfo.ps1
+++ getWindowsEventInfo.ps1
@@ -1,5 +1,5 @@
 $date = (Get-Date -F G).Replace('/','').Replace(' ','_').Replace(':','');
 
-GET-WinEvent System | Where-Object { ($_.Id -eq 42 -or $_.ProviderName -Match 'Power-Troubleshooter') -and $_.TimeCreated-ge (Get-Date).AddDays(-7) } | select-Object TimeCreated,Id | export-CSV "D:\documents\documents\202011_logicapps\$date.csv" -Encoding UTF8
+GET-WinEvent System | Where-Object { ($_.Id -eq 42 -or $_.ProviderName -Match 'Power-Troubleshooter') -and $_.TimeCreated-ge (Get-Date).AddDays(-7) } | select-Object TimeCreated,Id | export-CSV "[保存先のフォルダ指定]\$getWindowsEvent.csv" -Encoding UTF8
 
 
